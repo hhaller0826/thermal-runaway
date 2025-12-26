4d541ddefe596751482bb8a50fcf4faa548a35d1
hanita <hhaller@wisc.edu>
2025-12-26 17:53:19 -0500

    experiment results

--- experiment.py
+++ experiment.py
@@ -8,9 +8,13 @@ from src.data.train_test_split import get_divided_train_test
 if __name__ == "__main__":
 
     results_dir = "results"
-    exp_num = len(glob.glob(results_dir+"/reconstruction_errs_*.json"))
+    exp_num = len(glob.glob(results_dir+"/reconstruction_errs_*.json"))+1
+    save_to = f"{results_dir}/reconstruction_errs_unhealthy.json"
+    print(f"will save to {save_to}")
 
     _, unhealthy_files = get_divided_train_test(1, 'unhealthy')
+    # with open("trained_models/healthy_test_cells.json") as file:
+    #     healthy_files = json.load(file)
 
     device = "mps" if torch.mps.is_available() else "cpu"
     print("Using device:", device)
@@ -19,5 +23,5 @@ if __name__ == "__main__":
         model_filename="trained_models/3epoch_0.8healthy_500window_300step.pkl",
         filenames=unhealthy_files,
         device=device,
-        save_to=f"{results_dir}/reconstruction_errs_{exp_num}.json"
+        save_to=save_to
     )
--- src/validation.py
+++ src/validation.py
@@ -22,10 +22,10 @@ def evaluate_and_plot(filename, model, device='mps', window_size=1000, window_sk
     """
     Evaluate the given file and plot the results over the file's temperature data. 
     """
-    evaluations = evaluate_file_health(model, filename, device, window_size, window_skip, threshold)
+    evaluations = evaluate_file_health(filename, model, device, window_size, window_skip, threshold)
     plot_evals_on_temp(filename, evaluations, window_size, window_skip, sampling_rate)
 
-def plot_evals_on_temp(filename, evaluations, window_size, window_skip, sampling_rate, **kwargs):
+def plot_evals_on_temp(filename, evaluations, window_size, window_skip, sampling_rate, figsize=(10,5), xlim=None, ylim=None, **kwargs):
     """
     Plots the temperature data in the given file with dots indicating whether the data was classified as healthy
     or unhealthy at various timesteps.
@@ -38,29 +38,75 @@ def plot_evals_on_temp(filename, evaluations, window_size, window_skip, sampling
     x = np.arange(len(y)) / sampling_rate
 
     # Get evaluation colors
-    highlight_indices = []
-    highlight_colors = []
-    for i,e in enumerate(evaluations):
-        highlight_indices.append(i*window_skip + window_size)
-        if e: highlight_colors.append('red')
-        else: highlight_colors.append('blue')
+    colors = ['red' if e else 'blue' for e in evaluations]
 
     # Plot the line
-    plt.figure(figsize=(10, 5))
+    plt.figure(figsize=figsize)
     plt.plot(x, y, label='Data', color='gray')
 
     # Overlay colored dots
-    for idx, color in zip(highlight_indices, highlight_colors):
-        if idx >= len(y): break
-        plt.scatter(x[idx], y[idx], color=color, s=80, zorder=3, label=f'Index {idx}')
+    i = np.arange(len(colors))
+    idx = i * window_skip + window_size
+    idx = idx[:len(x)-window_size]
+    plt.scatter(x[idx], y[idx], c=colors[:len(x)-window_size], zorder=3)
+
+    if xlim is not None: plt.xlim(xlim)
+    if ylim is not None: plt.xlim(ylim)
 
     # Make it nice
-    plt.suptitle(f"{filename.split('/')[-1].split('.')[0]}")
+    plt.suptitle(f"{filename.split('/')[-1].split('.')[0]}", fontsize=16)
     plt.title(f"window_size={window_size}; window_skip={window_skip}", fontdict=SUBTITLE_FONT)
     plt.xlabel("Time (s)")
     plt.ylabel("Temp (°C)")
     plt.grid(alpha=0.3)
     plt.show()
+
+def draw_plot(ax, x, y, colors, window_skip, window_size, title=None):
+    ax.plot(x, y, label='Data', color='gray')
+
+    # Overlay colored dots
+    i = np.arange(len(colors))
+    idx = i * window_skip + window_size
+    ax.scatter(x[idx], y[idx], c=colors, s=5, zorder=3)
+
+    ax.set_title(title)
+    ax.set_xlabel("Time (s)")
+    ax.set_ylabel("Temp (°C)")
+    
+
+def plot_multiple_evals_on_temp(err_file, model=None, title="TEST DATA", filenames=None, model_filename="trained_models/3epoch_0.8healthy_500window_300step.pkl", threshold=300, sampling_rate=0.1, ncols=3, nrows=6, figsize=(30, 30), xlim=None, ylim=None):
+    with open(err_file, 'r') as file:
+        data = json.load(file)
+
+    if model is None:
+        model = TransformerAutoencoder.load(model_filename)
+
+    window_size=data["info"]["window_size"] 
+    window_skip=data["info"]["window_skip"]
+
+    fig, axes = plt.subplots(ncols=ncols, nrows=nrows, figsize=figsize)
+    fig.suptitle(f"{title}\nthreshold={threshold}; window_size={window_size}; window_skip={window_skip}")
+    idx = 0
+
+    if filenames is None: filenames = data["data"]
+    else: filenames = [f for f in data["data"] if f.split('/')[-1].split('.')[0] in filenames]
+
+    pbar = tqdm(filenames, desc="Drawing", leave=True)
+    for filename in pbar:
+        bd = BatteryData.load(filename)
+        temps = bd.timeseries_data[0].to_dict()['temperature_in_C']
+        temps = np.array(list(temps))
+        y = temps[~np.isnan(temps)]
+        x = np.arange(len(y)) / sampling_rate
+
+        colors = ['red' if err > threshold else 'blue' for err in data["data"][filename]]
+
+        curr_row = int(idx / ncols)
+        curr_col = int(idx % ncols)
+        name = filename.split('/')[-1].split('.pkl')[0]
+        ax = axes if len(filenames) == 1 else axes[curr_col] if nrows<2 else axes[curr_row, curr_col]
+        draw_plot(ax, x, y, colors, window_skip, window_size, title=name)
+        idx += 1
 
 
 # -----------------------------
@@ -107,6 +153,7 @@ def store_reconstruction_errors(
         'window_skip': window_skip, 
         'batch_size': batch_size, 
         'num_workers': num_workers,
+        'index_tuple': index_tuple
     }
 
     model = TransformerAutoencoder.load(model_filename)
